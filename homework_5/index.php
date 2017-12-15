123762581952509e825f6fe0aee7dfa0402a5fdb
Yuki-Amano <nikpnd@gmail.com>
2017-12-15 19:48:15 +0300

    версия 2.0

--- homework_5/index.php
+++ homework_5/index.php
@@ -1,51 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Телефонная книга</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-</head>
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="col-sx-12"><small>Задание 2.1</small>
-        <h1 class="text-center text-capitalize">Телефонная книга</h1>        
-    <table class="table table-striped">
-      <?php
-function getuserdata()
-{
-    $data = file_get_contents('./data-user.json', true);
-    $userdata = json_decode($data, true);
-    return $userdata ;
-}
-?>
-    <thead>
-        <tr>
-            <th>Имя</th>
-            <th>Фамилия</th>
-            <th>Адрес</th>
-            <th>Телефон</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php  foreach ( getuserdata() as $item ) {   ?>
-        <tr>
-            <td><?= $item['firstName'] ?></td>
-            <td><?= $item['lastName'] ?></td>
-              <td><?= $item['address'] ?></td>
-            <td><?= $item['phoneNumber'] ?></td>
-        </tr>
 <?php
-continue;
-return;  }	
-?>
-    </tbody>
-    </table>
-  </div>
-  </div>
-  </div>
-</body>
-</html>
+$data = file_get_contents('./data-user.json');
+$userdata = json_decode($data, true);
+?>	
+
+	<html>
+	<head>
+		<title>Записная книжка</title>
+	</head>
+	<body>
+		<table>
+			<tr>
+				<td>Имя</td>
+				<td>Фамилия</td>
+				<td>Адрес</td>
+				<td>Телефон</td>
+			</tr>
+			<?php foreach ($userdata as $items) { ?>
+			<tr>
+				<td><?= $items['firstName']; ?></td>
+				<td><?= $items['lastName']; ?></td>
+				<td><?= $items['address']; ?></td>
+				<td><?= $items['phoneNumber']; ?></td>
+			</tr>
+			<?php } ?>
+		</table>
+	</body>
+	</html>
+   

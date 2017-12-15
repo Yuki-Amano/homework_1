b9e23cba06851bfdda87525eec7eafc7ecc48de9
Yuki-Amano <nikpnd@gmail.com>
2017-12-15 19:33:51 +0300

    версия 2.0

--- homework_4/homework4.php
+++ homework_4/homework4.php
@@ -14,19 +14,41 @@ if (file_exists($cache_file) &&(time() - $cache_time) < filemtime($cache_file)) 
 
 else {
 $contents = file_get_contents($url);
-$clima=json_decode($contents);
+$clima=json_decode($contents, true);
 file_put_contents($cache_file, $contents);
 }
 
-$temp=$clima->main->temp;
-$humidity=$clima->main->humidity;
-$speed=$clima->wind->speed;
-$icon=$clima->weather[0]->icon.".png";
-$today = date("F j, Y, g:i a");
-
-echo "Сегодня в солнечной Чувашии (Чебоксары)" . " - " .$today . "<br>";
-echo "Температура воздуха: " . $temp ."&deg;C<br>";
-echo "Влажность воздуха: " . $humidity ."%<br>";
-echo "Скорость ветра: " . $speed ." м/с<br>";
-echo "<img src='http://openweathermap.org/img/w/" . $icon ."'/ >";
 ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Прогноз погоды</title>
+</head>
+
+<body>
+	<h1>Сегодня в солнечной Чувашии (Чебоксары) <?php echo date('d.m.o G:i') ?></h1>
+
+	<table>
+				<tr>
+					<td>Температура воздуха</td>
+					<td><?= $clima['main']['temp'] ?>°C</td>
+				</tr>        
+                <tr>
+                    <td>Влажность</td>
+                    <td><?= $clima['main']['humidity'] ?> %</td>
+                </tr>
+                <tr>
+                    <td>Восход</td>
+                    <td><?= date('G:i',$clima['sys']['sunrise']) ?> </td>
+                </tr>
+                <tr>
+                    <td>Закат</td>
+                    <td><?= date('G:i',$clima['sys']['sunset']) ?> </td>
+                </tr>
+    </table>
+	
+<img src="http://openweathermap.org/img/w/<?= $clima['weather']['0']['icon'] ?>.png">
+
+</body>
+</html>
